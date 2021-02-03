08508e1f50f53891d098165a3ee1443c5d02fd2e
minupalaniappan <minu@middesk.com>
2021-02-02 18:32:45 -0800

    Init app and set up blueprint

---
+++ src/js/helpers/api.js
@@ -0,0 +1,21 @@
+const fetchPlayerProfile = playerName => {}
+
+const fetchPlayByPlayForGame = gameLink => {}
+
+const fetchVideoForPlay = (gameId, eventId) => {}
+
+const parsePlayerStats = html => {}
+
+const parsePlayerLastFiveGames = html => {}
+
+const parsePlayerPlaysForGame = html => {}
+
+export {
+  fetchPlayerProfile,
+  fetchPlayByPlayForGame,
+  fetchVideoForPlay,
+  fetchVideoForPlay,
+  parsePlayerStats,
+  parsePlayerLastFiveGames,
+  parsePlayerPlaysForGame
+}
